9b5202113df0406b66e1f913214ed2b6c13c3f95
bsoghigian <bsoghigian@microsoft.com>
2022-05-28 09:02:42 -0700

    consumer and producer wrote

--- internal/server/http.go
+++ internal/server/http.go
@@ -3,12 +3,47 @@ package server
 import (
 	"encoding/json"
 	"net/http"
-	
+
 	"github.com/gorilla/mux"
 )
 
+func (s *httpServer) handleProduce(w http.ResponseWriter, r *http.Request) {
+	var req ProduceRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	res := ProduceResponse{Offset: off}
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-func NewHTTPServer(addr string) *http.Server{
+}
+
+func (s *httpServer) handleConsume(w http.ResponseWriter, r *http.Request) {
+
+	var req ConsumeRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	record, err := s.Log.Read(req.Offset)
+	if err == ErrOffsetNotFound {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, err, Error(), http.StatusInternalServerError)
+		return
+	}
+
+}
+
+func NewHTTPServer(addr string) *http.Server {
 
 	httpServer := newHTTPServer()
 	r := mux.NewRouter()
@@ -16,7 +51,7 @@ func NewHTTPServer(addr string) *http.Server{
 	r.HandleFunc("/", httpServer.handleProduce).Methods("POST")
 	r.HandleFunc("/", httpServer.handleConsume).Methods("GET")
 	return &http.Server{
-		Addr: addr,
+		Addr:    addr,
 		Handler: r,
 	}
 
@@ -41,4 +76,3 @@ type ConsumeRequest struct {
 type ConsumeResponse struct {
 	Record Record `json:"record"`
 }
-
