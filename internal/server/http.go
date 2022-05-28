6e57b794f044f8a169d185854d3778241970f38c
bsoghigian <bsoghigian@microsoft.com>
2022-05-28 09:08:11 -0700

    sheesh

--- internal/server/http.go
+++ internal/server/http.go
@@ -40,6 +40,12 @@ func (s *httpServer) handleConsume(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err, Error(), http.StatusInternalServerError)
 		return
 	}
+	res := ConsumeResponse{Record: record}
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 }
 
